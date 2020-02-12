4ab1b17798297ed3acc976c2baead00737856ca9
mahmudulazamshohan <rtc.shohan@gmail.com>
2020-02-12 17:06:33 +0600

    sign up successful

---
+++ database/factories/OtpFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Otp;
+use Faker\Generator as Faker;
+
+$factory->define(Otp::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
